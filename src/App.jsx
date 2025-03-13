6dc619f78aa7c9739c574cb03a2c62ec8dd608f3
BrayanMontenegro <montenegrobrayab45@gmail.com>
2025-03-13 09:18:53 -0600

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./database/authcontext";
+import ProtectedRoute from "./components/ProtectedRoute"; 
+import Login from "./view/Login";
+import Encabezado from "./components/Encabezado"
+import Inicio from "./view/Inicio";
+
 import './App.css'
 
 function App() {
+
   return (
     <>
-      <h1>Componente principal</h1>
+      <AuthProvider>
+        <Router>
+          <div className="App">
+            <Encabezado />
+            <main>
+              <Routes>
+                
+                <Route path="/" element={<Login />} />
+                <Route path="/inicio" element={<ProtectedRoute element={<Inicio />} />} />
+
+              </Routes>
+            </main>
+          </div>
+        </Router>
+      </AuthProvider>
     </>
-  ) 
+  )
 }
 
 export default App
